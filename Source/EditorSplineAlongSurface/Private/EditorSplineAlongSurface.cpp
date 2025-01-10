5ff3db2e119b067d15858e641e39480b395cc59b
ishikawash <ishikawash@gmail.com>
2025-01-10 16:49:31 +0900

    Add EditorSplineAlongSurface module.

---
+++ Source/EditorSplineAlongSurface/Private/EditorSplineAlongSurface.cpp
@@ -0,0 +1,17 @@
+#include "EditorSplineAlongSurface.h"
+
+#define LOCTEXT_NAMESPACE "FEditorSplineAlongSurfaceModule"
+
+void FEditorSplineAlongSurfaceModule::StartupModule()
+{
+
+}
+
+void FEditorSplineAlongSurfaceModule::ShutdownModule()
+{
+
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEditorSplineAlongSurfaceModule, EditorSplineAlongSurface)
